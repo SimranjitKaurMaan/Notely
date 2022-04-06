ee21b17565f2203dc8f56f7a3eaaf9e1dd17e495
Simranjit Kaur <simranjit.kaur@udaan.com>
2022-04-06 17:26:36 +0530

    Updated labels and notes model

--- src/backend/db/labels.js
+++ src/backend/db/labels.js
@@ -1,6 +1,19 @@
+import { v4 as uuid } from "uuid";
 /**
  * Labels Database can be added here.
  * You can add labels of your wish with different attributes
  * */
 
-export const labels = ['Work','Home','Personal']
+export const labels = [
+    {
+    _id: uuid(),
+    label: "Home",
+    description:
+      "literature in the form of prose, especially novels, that describes imaginary events and people"
+  },
+  {
+    _id: uuid(),
+    label: "Work",
+    description:
+      "literature in the form of prose, especially novels, that describes imaginary events and people"
+  }]
