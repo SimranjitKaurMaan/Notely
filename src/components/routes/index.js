b26af3d7abe3230454a481ac9672d0ce4342bb65
Simranjit Kaur <simranjit.kaur@udaan.com>
2022-04-07 22:17:34 +0530

    Change main page

--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -2,14 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import { Archive } from "../Archive";
 import { Home } from "../Home";
 import { Label } from "../Label";
-import { Main } from "../Main";
 import { Trash } from "../Trash";
 import { MockAPI } from "../../MockAPI";
 export const Router = () => {
     return (
         <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/labels" element={<Label />} />
         <Route path="/archive" element={<Archive />} />
         <Route path="/trash" element={<Trash />} />
