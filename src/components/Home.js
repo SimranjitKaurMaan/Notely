1dc73c0d3090cf8d2f848c5a9b2e4ace9e8ab6ab
Simranjit Kaur <simranjit.kaur@udaan.com>
2022-04-09 14:52:53 +0530

    Fixed notes event propagation

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,21 +3,21 @@ import { useNote } from '../contexts/note-context';
 import { Navbar, SearchBar, EmptyNote, Note } from './customComponents';
 
 export const Home = () => {
-    const {notes} = useNote();
+    const {notes, saveNote} = useNote();
     return <>
-    <div className="wrapper">
-    <aside>
-      <Navbar/>
-    </aside>
-        <main>
-             <div className="main-wrapper" onClick={(e) => e.stopPropagation()}>
-                    <SearchBar/>
-                    <div className="notes-wrapper">
-                        <EmptyNote/>
-                        {notes.map(note => <Note data={note}/>)}
-                    </div>
-            </div>
-        </main>
-    </div>
+        <div className="wrapper">
+        <aside>
+        <Navbar/>
+        </aside>
+            <main onClick={() => saveNote()}>
+                <div className="main-wrapper">
+                        <SearchBar/>
+                        <div className="notes-wrapper" onClick={(e) => e.stopPropagation()}>
+                            <EmptyNote/>
+                            {notes.map(note => <Note data={note}/>)}
+                        </div>
+                </div>
+            </main>
+        </div>
     </>
 }
