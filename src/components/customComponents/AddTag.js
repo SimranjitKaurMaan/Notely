e3bad00375a01c8459dd57fee2f8bf588d6a7bae
Simranjit Kaur <simranjit.kaur@udaan.com>
2022-04-11 05:52:35 +0530

    Handled checked unchecked tags

--- src/components/customComponents/AddTag.js
+++ src/components/customComponents/AddTag.js
@@ -6,8 +6,12 @@ export const AddTag = ({...props}) => {
     const {note , setNote} = useNote();
     const {setShowAddTags} = props;
     const labels = ['Work','Health','Creativity','Teams','Exercise','Chores'];
+    const handleLabelsChange = (label) => {
+        note.tags.includes(label) ? setNote({...note, tags: note.tags.filter(tag => tag !== label)}):setNote({...note, tags: [...note.tags, label]})
+    };
+
     return (<Box mb={4} sx={{...NotelyTheme.navBar.containerStyle, p: 2, borderRadius: 2, boxShadow: 1 }}>
-               {labels.map(label => <FormControlLabel control={<Checkbox/>} label={label} onChange={() => setNote({...note, tags: [...note.tags, label]})}/> )}
+               {labels.map(label => <FormControlLabel control={<Checkbox/>} label={label} checked={note.tags.includes(label)} onChange={() => handleLabelsChange(label)}/> )}
                <Button variant="outlined" size="small" onClick={() => setShowAddTags(false)}>CLOSE</Button>
             </Box>
     );
